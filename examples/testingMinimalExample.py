c7078035e0b071fef47a9e6c73399732d9ad962b
fsmMLK <fsmMLK@users.noreply.github.com>
2022-09-26 10:41:20 +0300

    documentation fixes

--- examples/testingMinimalExample.py
+++ examples/testingMinimalExample.py
@@ -6,7 +6,9 @@ import minimalExample as myExtension
 #loads the extension
 myExt = myExtension.MinimalExample()
 
-#runs
+#runs the plugin. Remember to change the paths of the svg files.
+# ``existing_file.svg`` is one existing file that will be modified by the example.
+# ``new_file.svg`` will be created with the result of the example.
 myExt.run([r'--drawCircle=True', r'--drawPlot=True', r'/path/to/existing_file.svg'], output=os.devnull)
 
 #save the result
